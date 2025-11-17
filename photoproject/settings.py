cb2fdbf446c50971dcafdcdde1c5ec01633b9aa6
ohr-mae <tky2502082@stu.o-hara.ac.jp>
2025-11-17 11:53:39 +0900

    Update settings.py

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--1fg^4n)nr%phzto1wv3751hn9d^^ff3s&8-63!i6a^1zj8+jw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['photosystem4taka.pythonanywhere.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # staticフォルダーのフルパスを設定
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
